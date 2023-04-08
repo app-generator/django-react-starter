ed685ebb2eb3d3bce9e32a8f6603a4f53328cce6
App Generator <51070104+app-generator@users.noreply.github.com>
2023-04-08 12:33:47 +0300

    Dummy

--- build.sh
+++ build.sh
@@ -11,5 +11,7 @@ python -m pip install --upgrade pip
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
+python manage.py makemigrations
 python manage.py migrate
+
+python manage.py collectstatic --no-input
